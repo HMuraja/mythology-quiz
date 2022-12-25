25f1e6780af5767d971d3348a2de664af952011b
HMuraja <hilla.muraja@hotmail.com>
2022-12-25 15:53:28 +0000

    Chnage HTML structure, create a new container inside the header and place navigation menu inside of it together with mute button. Add stack icon inside the newly created div. git push

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var headerLinks = document.getElementById("hidden-menu");
+    var menuContainer= document.getElementById("menu-container");
+    if (headerLinks.style.display === "block") {
+      headerLinks.style.display = "none";
+      menuContainer.style.backgroundColor = "beige";
+    } else {
+      headerLinks.style.display = "block";
+      menuContainer.style.backgroundColor = "whitesmoke";
+    }
+  }
