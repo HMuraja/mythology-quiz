c56aadcfb6dada2c22f2b76f701aa2f7e58cea76
HMuraja <hilla.muraja@hotmail.com>
2022-12-25 21:20:25 +0000

    Modify script in script.js file. Rename myFunction as dropMenu. Add change of stack icon to x when menu is opened

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function dropMenu() {
     var headerLinks = document.getElementById("hidden-menu");
     var menuContainer= document.getElementById("menu-container");
+    var stack= document.getElementById("stack-icon")
     if (headerLinks.style.display === "block") {
       headerLinks.style.display = "none";
       menuContainer.style.backgroundColor = "beige";
+      stack.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     } else {
       headerLinks.style.display = "block";
-      menuContainer.style.backgroundColor = "whitesmoke";    }
+      menuContainer.style.backgroundColor = "whitesmoke";
+      stack.innerHTML = `<i class="fa-solid fa-x"></i>`;
+        }
   }
