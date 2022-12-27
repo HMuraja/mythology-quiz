e2351ad0cca46bd140c36be0b7b8b16add8f9547
HMuraja <hilla.muraja@hotmail.com>
2022-12-27 23:55:58 +0000

    Create a new js file called questions and adcreate three arrays of questions for each difficulty. Include 8 objects into each array. Add question, answer options and correct answer into each object

---
+++ assets/js/questions.js
@@ -0,0 +1,108 @@
+let basicQuestions = [
+    {
+    question: "Which creature in Greek mythology was half-man and half-bull?",
+    answers:["Minotaur", "Centaur", "Faun"],
+    corectAnswer: "Minotaur"
+    },
+    {
+    question: "Olympians were cinsidered to be the major gods among greek mythology, how many where they?",
+    answers: ["6", "16", "12"],
+    corectAnswer: "12"
+    },
+    {
+    question: "Who was the beautiful youth that Greek goddess Aphrodite loved?",
+    answers: ["Eros", "Adonis", "Ganymede"],
+    corectAnswer: "Adonis"
+    },
+    {
+    question: "Which one of the following was both God of the Underworld and God of the Dead?",
+    answers: ["Hephaestus", "Hades", "Poseidon"],
+    corectAnswer: "Hades"
+    },
+    {
+    question: "Who was the god of love and love, lust, beauty, pleasure, passion,",
+    answers: ["Hera", "Aphrodite", "Athena"],
+    corectAnswer: "Aphrodite"
+    },
+    {
+    question: "Who was the god of war and courage, but also personifies brutality and bloodlust?",
+    answers: ["Athena", "Ares", "Kratos"],
+    corectAnswer: "Ares"
+    },
+    {
+    question: ´Artemis is greek goddess of what – only one among all gods?´,
+    answers: ["The Needing", "The Wanting", "The Yearning", "The Longing"],
+    corectAnswer: "The Longing"
+    }
+];
+
+let moderateQuestions = [
+    {
+    question: "According to legend, who fired the arrow that hit Achilles in the heel, his only vulnerable spot?",
+    answers:["Paris", "Hermes", "His mother"],
+    corectAnswer: "Paris"
+    },
+    {
+    question: "Who was the mythical Greek hunter after whom a constellation is named?",
+    answers: ["Orion", "Pleiades", "Ursa Major"],
+    corectAnswer: "Orion"
+    },
+    {
+    question: "What did Daedalus construct for King Minos so that he could keep the Minotaur there?",
+    answers: ["Golden Cage", "Labyrinth", "A walled enclosure"],
+    corectAnswer: "Labyrinth"
+    },
+    {
+    question: ´Whose son is Phaeton, who attempt to drive his father’s chariot, but is killed by a thunderbolt from Zeus?´,
+    answers: ["Hephaestus", "Apollo", "Hermes"],
+    corectAnswer: "Apollo"
+    },
+    {
+    question: "In Greek mythology, who was the goddess of the rainbow? ",
+    answers: ["Hebe", "Iris", "Electra"],
+    corectAnswer: "Iris"
+    },
+    {
+    question: "Artemis is greek goddess of what, only one among all gods?",
+    answers: ["Harvest and agriculture", "Virginity and chastity", "Hunt and music"],
+    corectAnswer: "Virginity and chastity"
+    }
+];
+
+let challengingQuestions = [
+    {
+    question: ´What was left in Pandora’s box after she released misery and evil?´,
+    answers:["Hope", "Wisdom", "Courage"],
+    corectAnswer: "Hope"
+    },
+    {
+    question:"Orpheus went into the underworld to rescue who?",
+    answers: ["Eurydice", "Persephone", "Aphrodite"],
+    corectAnswer: "Eurydice"
+    },
+    {
+    question: "Who fell in love with a statue he had carved?",
+    answers: ["Pygmalion", "Prometheus", "King Midas"],
+    corectAnswer: "Pygmalion"
+    },
+    {
+    question: "In greek mythology clio was the muse of what?",
+    answers: ["Poetry", "Dance", "History"],
+    corectAnswer: "History"
+    },
+    {
+    question: "Who was the titan who famously devoured his children, also the father of Zeus?",
+    answers: ["Cronus", "Uranus", "Pontus"],
+    corectAnswer: "Cronus"
+    },
+    {
+    question: "Parnassus was the sacred mountain home of the muses and which Greek god?",
+    answers: ["Athena", "Hephaistus", "Apollo"],
+    corectAnswer: "Apollo"
+    },
+    {
+    question: "Who were Erybus, the darkness and Nyx, the night?",
+    answers: ["First two creations born out of chaos", "Deities that were released from Pandoras box", "Children of Gaia"],
+    corectAnswer: "First two creations born out of chaos"
+    }
+];
