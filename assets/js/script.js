32eb6806dbd43c1a8461155dffa3000099a07004
HMuraja <hilla.muraja@hotmail.com>
2023-01-05 23:48:16 +0000

    Improved drop-down menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,16 +36,16 @@ function loadStartWindow(){
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function dropMenu() {
     var headerLinks = document.getElementById("hidden-menu");
-    var menuContainer= document.getElementById("menu-container");
+    var menuDrop= document.getElementById("menu-drop");
     var stack= document.getElementById("stack-icon")
     if (headerLinks.style.display === "block") {
       headerLinks.style.display = "none";
-      menuContainer.style.backgroundColor = "beige";
-      stack.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+      menuDrop.style.backgroundColor = "beige";
+      stack.innerHTML = `<i class="fa-solid fa-bars menu-icon"></i>`;
     } else {
       headerLinks.style.display = "block";
-      menuContainer.style.backgroundColor = "whitesmoke";
-      stack.innerHTML = `<i class="fa-solid fa-x"></i>`;
+      menuDrop.style.backgroundColor = "whitesmoke";
+      stack.innerHTML = `<i class="fa-solid fa-x menu-icon"></i>`;
         }
   }
 
